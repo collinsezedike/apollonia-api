b1a4357be2487a1016f23a0352c1be96cb13f9eb
Ezedike-egwom Collins <ezedikecollins@gmail.com>
2023-12-16 16:36:10 +0100

    feat: setup update and delete department by id endpoints

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,8 @@
 import addNewDepartment from "./department/addNewDepartment";
 import getAllDepartments from "./department/getAllDepartments";
 import getDepartmentById from "./department/getDepartmentById";
+import updateDepartmentById from "./department/updateDepartmentById";
+import deleteDepartmentById from "./department/deleteDepartmentById";
 
 import addNewStaff from "./staff/addNewStaff";
 import getAllStaff from "./staff/getAllStaff";
@@ -10,6 +12,8 @@ export {
   addNewDepartment,
   getAllDepartments,
   getDepartmentById,
+  updateDepartmentById,
+  deleteDepartmentById,
 
   // Staff
   addNewStaff,
--- src/routes/department.ts
+++ src/routes/department.ts
@@ -4,11 +4,17 @@ import {
   addNewDepartment,
   getAllDepartments,
   getDepartmentById,
+  updateDepartmentById,
+  deleteDepartmentById,
 } from "../controllers";
 
 const router = Router();
 
 router.route("/").get(getAllDepartments).post(addNewDepartment);
-router.route("/:department_id").get(getDepartmentById);
+router
+  .route("/:department_id")
+  .get(getDepartmentById)
+  .put(updateDepartmentById)
+  .delete(deleteDepartmentById);
 
 export default router;
